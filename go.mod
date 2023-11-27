64e0438988d09862ff1a6483fec4fbcac7c1f428
Borov <Michael.378826@ad.ccsd.net>
2023-11-27 14:16:33 -0800

    Trying out image proccessing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module parx.com/m
+
+go 1.21.4
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"image"
 	"image/color"
 	"image/png"
-	_ "image/png"
 	"os"
 )
 
@@ -15,13 +14,7 @@ type Pixel struct {
 	a uint8
 }
 
-// Function for basic error handling
-func check(err error, errCode string) {
-	if err != nil {
-		print(errCode)
-	}
-}
-
+// Function to be used for User Input/Config
 func main() {
 	//Read file
 	reader, err := os.Open("tester.png")
@@ -36,6 +29,67 @@ func main() {
 		panic(err)
 	}
 
+	pixArray := decodeImage(m)
+	pixArray = proccessImagePixels(pixArray)
+	encodeImage(m, pixArray)
+}
+
+// Basic error handling
+func check(err error, errCode string) {
+	if err != nil {
+		print(errCode)
+	}
+}
+
+// Sorting pixels into color groups
+func sortImagePixels(pixArray [][]Pixel) [][]Pixel {
+	pixArray = proccessImagePixels(pixArray)
+	return pixArray
+}
+
+// Sort pixels into arrays of similar pixels (to reduce load on sorting algorithm)
+// Merge back into an 2D array (could be just a 1D array, but 2D makes for easier proccessing!)
+func proccessImagePixels(pixArray [][]Pixel) [][]Pixel {
+	sortedArray := [][]Pixel{}
+
+	for y := 0; y < len(pixArray); y++ {
+		for x := 0; x < len(pixArray[y]); x++ {
+			pixel := pixArray[x][y]
+			for i := 0; i < len(sortedArray); i++ {
+				if ((pixel.r <= sortedArray[i][0].r+15) || (pixel.r >= sortedArray[i][0].r-15)) &&
+					((pixel.g <= sortedArray[i][0].g+15) || (pixel.g >= sortedArray[i][0].g-15)) &&
+					((pixel.b <= sortedArray[i][0].b+15) || (pixel.b >= sortedArray[i][0].b-15)) {
+
+					alpha := pixel.a
+					for j := 0; j < len(sortedArray[i]); j++ {
+						if alpha >= sortedArray[i][j].a {
+							sortedArray[i] = insert(sortedArray[i], j, pixel)
+						}
+					}
+					break
+				}
+
+				if i == len(sortedArray) {
+					sortedArray = append(sortedArray, []Pixel{pixel})
+				}
+			}
+
+		}
+	}
+	return pixArray
+}
+
+func insert(a []Pixel, index int, value Pixel) []Pixel {
+	if len(a) == index { // nil or empty slice or after last element
+		return append(a, value)
+	}
+	a = append(a[:index+1], a[index:]...) // index < len(a)
+	a[index] = value
+	return a
+}
+
+// Convert file into 2D Array of Pixels
+func decodeImage(m image.Image) [][]Pixel {
 	//Define dimensions of image
 	bounds := m.Bounds()
 
@@ -54,22 +108,28 @@ func main() {
 		}
 	}
 
+	return pixArray
+}
+
+// Convert 2D Array of Pixels into Image
+func encodeImage(m image.Image, pixArray [][]Pixel) {
 	//Get Dimensions of image and create a new image for output based on dimensions
 	//Image is defined as a rectangle with W: Max.X and L: Max.Y based on dimensions
+	bounds := m.Bounds()
 	origin := image.Point{}
 	last := image.Point{X: bounds.Max.X, Y: bounds.Max.Y}
 	img := image.NewRGBA(image.Rectangle{Min: origin, Max: last})
 
 	//Populate output with pixels from pixArray
-	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			currPix := pixArray[y][x]
-			img.SetRGBA(bounds.Max.X-x-1, bounds.Max.Y-y-1, color.RGBA{R: currPix.r, G: currPix.g, B: currPix.b, A: currPix.a})
+			img.SetRGBA(x, y, color.RGBA{R: currPix.r, G: currPix.g, B: currPix.b, A: currPix.a})
 		}
 	}
 
 	//Create file for output
-	output, _ := os.Create("output.png")
+	output, err := os.Create("output.png")
 
 	//Write output image to "output.png" and encode to .png format
 	err = png.Encode(output, img)
